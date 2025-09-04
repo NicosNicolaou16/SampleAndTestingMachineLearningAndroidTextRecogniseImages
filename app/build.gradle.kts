10433579e45fcd47caff3d33db606465671bb179
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-09-04 23:19:30 +0300

    update Target SDK to version 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,13 @@ plugins {
 
 android {
     namespace = "com.nicos.sampleandtestingmachinelearningandroidtextrecogniseimages"
-    buildToolsVersion = "35.0.0"
-    compileSdk = 35
+    buildToolsVersion = "36.0.0"
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.nicos.sampleandtestingmachinelearningandroidtextrecogniseimages"
-        minSdk = 28
-        targetSdk = 35
+        minSdk = 29
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
