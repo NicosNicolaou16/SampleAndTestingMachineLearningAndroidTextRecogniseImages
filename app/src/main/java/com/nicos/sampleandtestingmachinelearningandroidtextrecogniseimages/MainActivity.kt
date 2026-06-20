b1e7787e9f4bdc8cb065e3593ae91416d5ce4424
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-21 00:55:36 +0300

    minor fixes

--- app/src/main/java/com/nicos/sampleandtestingmachinelearningandroidtextrecogniseimages/MainActivity.kt
+++ app/src/main/java/com/nicos/sampleandtestingmachinelearningandroidtextrecogniseimages/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -48,6 +49,7 @@ import com.nicos.sampleandtestingmachinelearningandroidtextrecogniseimages.ui.th
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             SampleAndTestingMachineLearningAndroidTextRecogniseImagesTheme {
                 // A surface container using the 'background' color from the theme
